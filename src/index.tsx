ce1c9570a14606f1b2aab0a3d08e4f0ae19488ee
knoll3 <kyle.noll@decentlabs.io>
2022-08-04 23:26:57 -0400

    Add sentry config to app

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import * as Sentry from '@sentry/react';
+import { BrowserTracing } from '@sentry/tracing';
+
+// Used for remote error reporting
+// https://sentry.io/organizations/decent-mg/issues/
+Sentry.init({
+  dsn: process.env.REACT_APP_SENTRY_DSN || '',
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-      <App />
+    <App />
   </React.StrictMode>
 );
-
