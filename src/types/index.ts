be236ac5eb37c388df9af20eb694b947608b3cd9
Kellar <kellar.base09@gmail.com>
2022-08-16 17:42:00 +0100

    duplicate fix

--- src/types/index.ts
+++ src/types/index.ts
@@ -27,18 +27,6 @@ export interface SignatureWithAccount extends Signature {
   account: string;
 }
 
-export interface ContractArchaeologist {
-  archAddress: string;
-  storageFee: BigNumber;
-  diggingFee: BigNumber;
-  bounty: BigNumber;
-  hashedShard: string;
-}
-
-export interface SignatureWithAccount extends Signature {
-  account: string;
-}
-
 // Temporary
 export interface Sarcophagus {
   id: string;
