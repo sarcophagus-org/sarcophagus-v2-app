2ff08bdf6f9aff1ddc774602970706077b1a4c9c
knoll3 <kyle.noll@decentlabs.io>
2022-08-18 10:40:06 -0400

    Fix webpack error

--- config-overrides.js
+++ config-overrides.js
@@ -1,11 +1,23 @@
 const webpack = require('webpack');
+
 module.exports = function override(config) {
   config.resolve.fallback = {
     buffer: require.resolve('buffer'),
+    crypto: require.resolve('crypto-browserify'),
+    stream: require.resolve('stream-browserify'),
+    assert: require.resolve('assert'),
+    http: require.resolve('stream-http'),
+    https: require.resolve('https-browserify'),
+    os: require.resolve('os-browserify'),
+    url: require.resolve('url'),
+    zlib: require.resolve('browserify-zlib'),
+    process: require.resolve('process'),
   };
   config.plugins.push(
     new webpack.ProvidePlugin({
       Buffer: ['buffer', 'Buffer'],
+      // https://github.com/framer/motion/issues/1504#issuecomment-1092838443
+      process: 'process/browser.js',
     })
   );
 
