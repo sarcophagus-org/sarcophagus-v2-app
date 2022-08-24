eb090d11df052d9cd2f0dfa1f7491d9f8967231f
mobiusTripper-crypto <99622829+mobiusTripper-crypto@users.noreply.github.com>
2022-08-24 16:45:06 -0400

    refactor lookup function

---
+++ src/components/lookupPublicKey/index.tsx
@@ -0,0 +1,58 @@
+import { VStack, Heading, Input, FormLabel, Button, Text } from '@chakra-ui/react';
+import { useState, useRef } from 'react';
+import { useLookupPublicKey, LookupPublicKeyStatus } from './useLookupPublicKey';
+
+export function LookupPublicKey() {
+  console.log('rerender');
+  const [address, setAddress] = useState('');
+  const [publicKey, setPublicKey] = useState('');
+
+  const { lookupPublicKey, lookupStatus } = useLookupPublicKey();
+
+  const getLookupStatusMessage = (status: LookupPublicKeyStatus | null): string => {
+    switch (status) {
+      case LookupPublicKeyStatus.SUCCESS:
+        return 'Successfully retrieved public key';
+      case LookupPublicKeyStatus.ERROR:
+        return 'Error retrieving public key';
+      case LookupPublicKeyStatus.NO_TRANSACTIONS:
+        return 'No transactions available, cannot lookup public key';
+      case LookupPublicKeyStatus.LOADING:
+        return 'Loading...';
+      case LookupPublicKeyStatus.INVALID_ADDRESS:
+        return 'Invalid address';
+      case LookupPublicKeyStatus.NOT_CONNECTED:
+        return 'Wallet not connected';
+      case LookupPublicKeyStatus.WRONG_NETWORK:
+        return 'Wallet connected to incorrect network';
+      default:
+        return '';
+    }
+  };
+
+  async function handleOnClick(): Promise<void> {
+    const pk = await lookupPublicKey(address);
+    setPublicKey(pk || 'not found');
+  }
+
+  return (
+    <VStack align="left">
+      <Heading>Lookup Public Key</Heading>
+      <FormLabel>Etherum Address</FormLabel>
+      <Input
+        placeholder="0x0..."
+        onChange={e => setAddress(e.currentTarget.value)}
+        value={address}
+      />
+      <Button
+        background="gray"
+        width={20}
+        onClick={handleOnClick}
+      >
+        Submit
+      </Button>
+      <Text>Status:{getLookupStatusMessage(lookupStatus)}</Text>
+      <Text>Public Key:{publicKey}</Text>
+    </VStack>
+  );
+}
---
+++ src/components/lookupPublicKey/useLookupPublicKey.ts
@@ -0,0 +1,120 @@
+import { ethers, UnsignedTransaction } from 'ethers';
+import { TransactionResponse } from '@ethersproject/providers';
+import { useState } from 'react';
+import axios from 'axios';
+import { useProvider, useNetwork } from 'wagmi';
+
+export enum LookupPublicKeyStatus {
+  SUCCESS,
+  ERROR,
+  NO_TRANSACTIONS,
+  LOADING,
+  INVALID_ADDRESS,
+  WRONG_NETWORK,
+  NOT_CONNECTED,
+}
+
+const etherscanEndpoint = 'https://api.etherscan.io/api';
+const etherscanApikey = 'KJZI2B4F3RXBWN6YJMACXARJ5YBEK9WMJV';
+const getParameters =
+  'module=account&action=txlist&startblock=0&endblock=99999999&page=1&offset=1000&sort=asc';
+
+export function useLookupPublicKey() {
+  const [lookupStatus, setLookupStatus] = useState<LookupPublicKeyStatus | null>(null);
+  const provider = useProvider();
+  const { chain } = useNetwork();
+
+  async function recoverPublicKeyFromTransaction(transactionIn: TransactionResponse) {
+    const transactionOut: UnsignedTransaction = {
+      type: transactionIn.type,
+      nonce: transactionIn.nonce,
+      gasLimit: transactionIn.gasLimit,
+      to: transactionIn.to,
+      value: transactionIn.value,
+      data: transactionIn.data,
+
+      ...(transactionIn.chainId && { chainId: transactionIn.chainId }),
+
+      ...((transactionIn.type === 0 || transactionIn.type === 1 || !transactionIn.type) && {
+        gasPrice: transactionIn.gasPrice,
+      }),
+
+      ...((transactionIn.type === 1 || transactionIn.type === 2) && {
+        accessList: transactionIn.accessList,
+      }),
+
+      ...(transactionIn.type === 2 && {
+        maxFeePerGas: transactionIn.maxFeePerGas,
+        maxPriorityFeePerGas: transactionIn.maxPriorityFeePerGas,
+      }),
+    };
+
+    const resolvedTx = await ethers.utils.resolveProperties(transactionOut);
+    const rawTx = ethers.utils.serializeTransaction(resolvedTx);
+    const msgHash = ethers.utils.keccak256(rawTx);
+
+    const signature = ethers.utils.splitSignature({
+      r: transactionIn.r || '',
+      s: transactionIn.s || '',
+      v: transactionIn.v || 0,
+    });
+
+    const publicKey = ethers.utils.recoverPublicKey(msgHash, signature);
+
+    console.log(transactionOut, ethers.utils.computeAddress(publicKey));
+
+    return ethers.utils.computeAddress(publicKey) == transactionIn.from ? publicKey : undefined;
+  }
+
+  async function lookupPublicKey(address: string): Promise<string | undefined> {
+    setLookupStatus(LookupPublicKeyStatus.LOADING);
+    if (!ethers.utils.isAddress(address)) {
+      setLookupStatus(LookupPublicKeyStatus.INVALID_ADDRESS);
+      return;
+    }
+
+    if (chain) {
+      if (chain.id !== 1) {
+        setLookupStatus(LookupPublicKeyStatus.WRONG_NETWORK);
+        return;
+      }
+    } else {
+      setLookupStatus(LookupPublicKeyStatus.NOT_CONNECTED);
+      return;
+    }
+
+    const transactionCount = await provider.getTransactionCount(address);
+    if (transactionCount === 0) {
+      setLookupStatus(LookupPublicKeyStatus.NO_TRANSACTIONS);
+      return;
+    }
+
+    const response = await axios.get(
+      `${etherscanEndpoint}?${getParameters}&address=${address}&apikey=${etherscanApikey}`
+    );
+    const status = response.data.status;
+    if (status !== '1') {
+      setLookupStatus(LookupPublicKeyStatus.ERROR);
+      return;
+    }
+
+    for (let index = 0; index < response.data.result.length; index++) {
+      const transaction = await provider.getTransaction(response.data.result[index].hash);
+
+      //we can only resolve a public key when the 'from' transaction matches the given address
+      if (transaction.from.toLowerCase() === address.toLowerCase()) {
+        const publicKey = await recoverPublicKeyFromTransaction(transaction);
+        // if (publicKey) {
+        //   setLookupStatus(LookupPublicKeyStatus.SUCCESS);
+        //   return publicKey;
+        // }
+        //        console.log(transaction, publicKey);
+      }
+    }
+
+    setLookupStatus(LookupPublicKeyStatus.NO_TRANSACTIONS);
+    return;
+  }
+
+  return { lookupStatus, lookupPublicKey };
+}
