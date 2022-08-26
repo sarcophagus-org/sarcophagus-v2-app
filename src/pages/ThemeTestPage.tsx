5fdb1b733c241b09e4d4fd337fa21410f1a7b6cd
knoll3 <kyle.noll@decentlabs.io>
2022-08-25 21:37:45 -0400

    Add Text default styling

--- src/pages/ThemeTestPage.tsx
+++ src/pages/ThemeTestPage.tsx
@@ -1,11 +1,22 @@
-import { VStack, Text, Heading } from '@chakra-ui/react';
+import { VStack, Text, Heading, Button } from '@chakra-ui/react';
 
 export function ThemeTestPage() {
   return (
-    <VStack align="left">
+    <VStack
+      align="left"
+      spacing={6}
+    >
       {/* <Heading style={{ fontSize: 24 }}>Header test</Heading> */}
-      <Heading>Header test</Heading>
-      <Text>Testing some text</Text>
+      <Heading>This is a Header</Heading>
+      <Text>This is some primary text</Text>
+      <Text variant="secondary">This is some secondary text</Text>
+      <Button w={200}>Clicky Button</Button>
+      <Button
+        w={200}
+        variant="link"
+      >
+        Clicky Link
+      </Button>
     </VStack>
   );
 }
