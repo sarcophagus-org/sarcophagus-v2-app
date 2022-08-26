39723f7b80f6e67ad52ccc849034797f123f4d7e
knoll3 <kyle.noll@decentlabs.io>
2022-08-25 20:49:17 -0400

    Add some colors

---
+++ src/theme/colors.ts
@@ -0,0 +1,33 @@
+// Contains all the colors used by the sarcophagus app
+
+export const colors = {
+  // Colors from white to black
+  brand: {
+    0: '#ffffff',
+    50: '#f2f2f2',
+    100: '#d9d9d9',
+    200: '#bfbfbf',
+    300: '#a6a6a6',
+    400: '#8c8c8c',
+    500: '#737373',
+    600: '#595959',
+    700: '#404040',
+    800: '#262626',
+    900: '#0d0d0d',
+    950: '#000000',
+  },
+
+  // Violet colors for some borders and some secondary text. Only a couple of these are used.
+  violet: {
+    50: '#f3f0f9',
+    100: '#d7d5dd',
+    200: '#bdb9c4',
+    300: '#a29dac',
+    400: '#878194',
+    500: '#6e687b',
+    600: '#565160',
+    700: '#3e3945',
+    800: '#25222a',
+    900: '#0d0a12',
+  },
+};
