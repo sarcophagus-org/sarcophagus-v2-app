38b6d217bb16e36f73617a04ebcaab823c1ad5d7
knoll3 <kyle.noll@decentlabs.io>
2022-08-27 15:05:51 -0400

    Modify steps to match newer design

--- src/features/embalm/stepNavigator/stepElement/StepStatusIndicator.tsx
+++ src/features/embalm/stepNavigator/stepElement/StepStatusIndicator.tsx
@@ -3,37 +3,51 @@ import { StepStatus } from 'store/embalm/reducer';
 
 interface StepStatusIndicatorProps {
   status: StepStatus;
+  index: number;
 }
 
-export function StepStatusIndicator({ status }: StepStatusIndicatorProps) {
+export function StepStatusIndicator({ status, index }: StepStatusIndicatorProps) {
   const stepStatusMap = {
     [StepStatus.Complete]: (
       <Flex
-        h="20px"
-        w="20px"
+        h="24px"
+        w="24px"
         border="1px solid"
         borderColor="brand.950"
-        backgroundColor="brand.500"
+        backgroundColor="brand.950"
         borderRadius={100}
-      />
+        align="center"
+        justifyContent="center"
+        color="brand.0"
+      >
+        {index}
+      </Flex>
     ),
     [StepStatus.Started]: (
       <Flex
-        h="20px"
-        w="20px"
+        h="24px"
+        w="24px"
         border="1px solid"
         borderColor="brand.950"
         borderRadius={100}
-      />
+        align="center"
+        justifyContent="center"
+      >
+        {index}
+      </Flex>
     ),
     [StepStatus.NotStarted]: (
       <Flex
-        h="20px"
-        w="20px"
+        h="24px"
+        w="24px"
         border="1px solid"
         borderColor="brand.950"
         borderRadius={100}
-      />
+        align="center"
+        justifyContent="center"
+      >
+        {index}
+      </Flex>
     ),
   };
 
