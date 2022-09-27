58d2c8ee4f421a5778a0a8de18a142fa05df136f
knoll3 <kyle.noll@decentlabs.io>
2022-09-27 15:53:16 -0400

    Get all arch profiles in single call

--- src/features/embalm/stepContent/hooks/useLoadArchaeologists.ts
+++ src/features/embalm/stepContent/hooks/useLoadArchaeologists.ts
@@ -4,7 +4,6 @@ import { useEffect } from 'react';
 import { startLoad, stopLoad } from 'store/app/actions';
 import { setArchaeologists } from 'store/embalm/actions';
 import { useDispatch, useSelector } from 'store/index';
-import { ArchaeologistProfile } from 'types';
 import { readContract } from 'wagmi/actions';
 
 /**
@@ -24,41 +23,25 @@ export function useLoadArchaeologists() {
         if (archaeologists.length > 0) return;
         dispatch(startLoad());
 
-        let profiles: Record<string, ArchaeologistProfile> = {};
-
         const addresses: string[] = await readContract({
           addressOrName: networkConfig.diamondDeployAddress,
           contractInterface: ViewStateFacet.abi,
           functionName: 'getArchaeologistProfileAddresses',
         });
+        if (!addresses) return;
 
-        if (addresses) {
-          // TODO: Having to do single use `readContract`s in a loop because `useContractReads` does not work in hardhat
-          for await (const addr of addresses) {
-            const profile: ArchaeologistProfile = await readContract({
-              addressOrName: networkConfig.diamondDeployAddress,
-              contractInterface: ViewStateFacet.abi,
-              functionName: 'getArchaeologistProfile',
-              args: [addr],
-            });
+        const profiles = await readContract({
+          addressOrName: networkConfig.diamondDeployAddress,
+          contractInterface: ViewStateFacet.abi,
+          functionName: 'getArchaeologistProfiles',
+          args: [addresses],
+        });
 
-            if (profile) {
-              profiles[addr] = {
-                archAddress: addr,
-                exists: profile.exists,
-                minimumDiggingFee: profile.minimumDiggingFee,
-                maximumRewrapInterval: profile.maximumRewrapInterval,
-                peerId: profile.peerId,
-              };
-            }
-          }
-
-          const newArchaeologists = Object.values(profiles).map(p => ({
-            profile: p,
-            isOnline: false,
-          }));
-          dispatch(setArchaeologists(newArchaeologists));
-        }
+        const newArchaeologists = profiles.map((p, i) => ({
+          profile: { ...p, archAddress: addresses[i] },
+          isOnline: false,
+        }));
+        dispatch(setArchaeologists(newArchaeologists));
       } catch (error) {
         console.error(error);
       } finally {
