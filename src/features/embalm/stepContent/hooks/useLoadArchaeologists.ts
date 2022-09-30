788d6d71307d72d9babdcf9d5024eaae0d84ffda
mobiusTripper-crypto <99622829+mobiusTripper-crypto@users.noreply.github.com>
2022-09-30 17:42:14 -0400

    fixed useLoadArchaeologist

--- src/features/embalm/stepContent/hooks/useLoadArchaeologists.ts
+++ src/features/embalm/stepContent/hooks/useLoadArchaeologists.ts
@@ -24,42 +24,26 @@ export function useLoadArchaeologists() {
         if (archaeologists.length > 0) return;
         dispatch(startLoad());
 
-        let profiles: Record<string, ArchaeologistProfile> = {};
-
         const addresses: string[] = await readContract({
           addressOrName: networkConfig.diamondDeployAddress,
           contractInterface: ViewStateFacet.abi,
           functionName: 'getArchaeologistProfileAddresses',
         });
 
-        if (addresses?.length !== 0) {
-          // TODO: Having to do single use `readContract`s in a loop because `useContractReads` does not work in hardhat
-          for await (const addr of addresses) {
-            const profile: ArchaeologistProfile = await readContract({
-              addressOrName: networkConfig.diamondDeployAddress,
-              contractInterface: ViewStateFacet.abi,
-              functionName: 'getArchaeologistProfile',
-              args: [addr],
-            });
+        if (!addresses || addresses.length === 0) return;
 
-            if (profile) {
-              profiles[addr] = {
-                archAddress: addr,
-                exists: profile.exists,
-                minimumDiggingFee: profile.minimumDiggingFee,
-                maximumRewrapInterval: profile.maximumRewrapInterval,
-                peerId: profile.peerId,
-                signature: { v: 0, r: '', s: '' }, // TODO Update with signature from profile
-              };
-            }
-          }
+        const profiles = await readContract({
+          addressOrName: networkConfig.diamondDeployAddress,
+          contractInterface: ViewStateFacet.abi,
+          functionName: 'getArchaeologistProfiles',
+          args: [addresses],
+        });
 
-          const newArchaeologists = Object.values(profiles).map(p => ({
-            profile: p,
-            isOnline: false,
-          }));
-          dispatch(setArchaeologists(newArchaeologists));
-        }
+        const newArchaeologists = profiles.map((p, i) => ({
+          profile: { ...p, archAddress: addresses[i] },
+          isOnline: false,
+        }));
+        dispatch(setArchaeologists(newArchaeologists));
       } catch (error) {
         console.error(error);
       } finally {
