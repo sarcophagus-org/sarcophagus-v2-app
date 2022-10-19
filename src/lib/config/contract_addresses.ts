cfed9408733c66b7679b64ff2bdbca4ac324d911
Kellar <kellar.base09@gmail.com>
2022-10-19 19:37:45 +0100

    Update contract_addresses.ts

--- src/lib/config/contract_addresses.ts
+++ src/lib/config/contract_addresses.ts
@@ -4,4 +4,4 @@ const envTokenAddresses = process.env.REACT_APP_TOKEN_ADDRESSES;
 export const contractAddresses = !envContractAddresses ? {} : JSON.parse(envContractAddresses) as Record<string, string>;
 export const tokenAddresses = !envTokenAddresses ? {} : JSON.parse(envTokenAddresses) as Record<string, string>;
 
-export const supportedChainIds = Object.keys(contractAddresses).map(Number.parseInt);
+export const supportedChainIds = Object.keys(contractAddresses).map(chainId => Number.parseInt(chainId));
