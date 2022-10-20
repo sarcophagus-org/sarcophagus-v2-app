02a82a2ff837de7d78c487c94c46b24dcb04df90
knoll3 <kyle.noll@decentlabs.io>
2022-10-20 17:28:14 -0400

    Build create sarcophagus progress tracker

---
+++ src/features/embalm/stepContent/components/ProgressTrackerStage.tsx
@@ -0,0 +1,44 @@
+import { Flex, Spinner, Text } from '@chakra-ui/react';
+import { StatusIndicator } from './StatusIndicator';
+
+export enum StageStatus {
+  NOT_STARTED = 'NOT_STARTED',
+  IN_PROGRESS = 'IN_PROGRESS',
+  COMPLETE = 'COMPLETE',
+}
+
+export interface ProgressTrackerStageProps {
+  stageStatus?: StageStatus;
+  index?: number;
+  children: React.ReactNode;
+}
+
+export function ProgressTrackerStage({
+  stageStatus = StageStatus.NOT_STARTED,
+  index = 0,
+  children,
+}: ProgressTrackerStageProps) {
+  return (
+    <Flex
+      alignItems="center"
+      justifyContent="space-between"
+    >
+      <Flex>
+        <StatusIndicator
+          status={stageStatus}
+          index={index}
+        />
+        <Text
+          ml={3}
+          fontSize="md"
+          color={stageStatus === StageStatus.NOT_STARTED ? 'brand.500' : 'brand.950'}
+        >
+          {children}
+        </Text>
+      </Flex>
+      {stageStatus === StageStatus.IN_PROGRESS && <Spinner size="sm" />}
+      {/* If we want to show additional information on the stage, we can conditionally add it here. */}
+    </Flex>
+  );
+  //
+}
---
+++ src/features/embalm/stepContent/components/StatusIndicator.tsx
@@ -0,0 +1,62 @@
+import { CheckIcon } from '@chakra-ui/icons';
+import { Flex, Text } from '@chakra-ui/react';
+import { StageStatus } from './ProgressTrackerStage';
+
+interface StatusIndicatorProps {
+  status: StageStatus;
+  index: number;
+}
+
+export function StatusIndicator({ status, index }: StatusIndicatorProps) {
+  const statusSize = '25px';
+
+  const stageStatusMap = {
+    [StageStatus.COMPLETE]: (
+      <Flex
+        h={statusSize}
+        w={statusSize}
+        border="1px solid"
+        borderColor="controversialBlue"
+        backgroundColor="controversialBlue"
+        borderRadius={100}
+        align="center"
+        justifyContent="center"
+      >
+        <CheckIcon fontSize="xs" />
+      </Flex>
+    ),
+    [StageStatus.IN_PROGRESS]: (
+      <Flex
+        h={statusSize}
+        w={statusSize}
+        border="1px solid"
+        borderColor="brand.950"
+        borderRadius={100}
+        align="center"
+        justifyContent="center"
+      >
+        <Text fontSize="sm">{index + 1}</Text>
+      </Flex>
+    ),
+    [StageStatus.NOT_STARTED]: (
+      <Flex
+        h={statusSize}
+        w={statusSize}
+        border="1px solid"
+        borderColor="brand.500"
+        borderRadius={100}
+        align="center"
+        justifyContent="center"
+      >
+        <Text
+          fontSize="sm"
+          color="brand.500"
+        >
+          {index + 1}
+        </Text>
+      </Flex>
+    ),
+  };
+
+  return stageStatusMap[status];
+}
