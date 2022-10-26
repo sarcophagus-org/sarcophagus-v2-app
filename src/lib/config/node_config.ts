fab661b51693963de4e7c4045e0141c2a3bec173
sethhrbek <seth.hrbek@gmail.com>
2022-10-26 11:50:11 -0400

    upgrade libp2p packages

--- src/lib/config/node_config.ts
+++ src/lib/config/node_config.ts
@@ -1,7 +1,7 @@
-import { Noise } from '@chainsafe/libp2p-noise';
-import { Mplex } from '@libp2p/mplex';
-import { KadDHT } from '@libp2p/kad-dht';
-import { WebRTCStar } from '@libp2p/webrtc-star';
+import { noise } from '@chainsafe/libp2p-noise';
+import { mplex } from '@libp2p/mplex';
+import { kadDHT } from '@libp2p/kad-dht';
+import { webRTCStar } from '@libp2p/webrtc-star';
 
 // protocol names used to set up communication with archaeologist nodes
 // these values must be the same for webapp's and archaeologist's node config
@@ -9,12 +9,12 @@ export const PUBLIC_KEY_STREAM = '/archaeologist-public-key';
 export const NEGOTIATION_SIGNATURE_STREAM = '/archaeologist-negotiation-signature';
 const DHT_PROTOCOL_PREFIX = '/archaeologist-service';
 
-const dht = new KadDHT({
+const dht = kadDHT({
   protocolPrefix: DHT_PROTOCOL_PREFIX,
   clientMode: false,
 });
 
-const webRtcStar = new WebRTCStar();
+const webRtcStar = webRTCStar();
 
 export const nodeConfig: any = {
   addresses: {
@@ -28,9 +28,9 @@ export const nodeConfig: any = {
         return `/dns4/${server}/tcp/443/wss/p2p-webrtc-star`;
       }),
   },
-  transports: [webRtcStar],
-  connectionEncryption: [new Noise()],
-  streamMuxers: [new Mplex()],
+  transports: [webRtcStar.transport],
+  connectionEncryption: [noise()],
+  streamMuxers: [mplex()],
   peerDiscovery: [webRtcStar.discovery],
   dht,
   connectionManager: {
