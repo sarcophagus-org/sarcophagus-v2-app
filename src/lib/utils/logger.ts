9580db8c48476766682701c45de4e796537e6994
Breads <breads.@Breadss-MacBook-Pro.local>
2022-10-26 12:27:19 -0600

    use the env specific logger in recover key hook

--- src/lib/utils/logger.ts
+++ src/lib/utils/logger.ts
@@ -1,3 +1,4 @@
-export const log = (output: string) => {
-  console.info(output);
+export const log = (...args: any[]) => {
+  if (process.env.NODE_ENV !== 'production')
+    console.info(...args);
 };
