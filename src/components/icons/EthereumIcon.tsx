7fdbc881e6991ad6564ee8c63d2b05b3a9b61792
knoll3 <kyle.noll@decentlabs.io>
2022-11-09 00:58:13 -0500

    Build Bundlr page

---
+++ src/components/icons/EthereumIcon.tsx
@@ -0,0 +1,13 @@
+import { Icon, IconProps } from '@chakra-ui/react';
+
+// SVG token from master figma file
+export function EthereumIcon(props: IconProps) {
+  return (
+    <Icon {...props}>
+      <path
+        d="M7.94841 17.6584L0.964844 13.5371L7.94415 23.3758L14.9313 13.5371L7.94415 17.6584H7.94841ZM8.05221 0.625L1.07148 12.2112L8.05221 16.3396L15.0358 12.2155L8.05221 0.625Z"
+        fill="white"
+      />
+    </Icon>
+  );
+}
