dcffc29bfdf32c3ca4b36214647f9c9ee4d767d0
knoll3 <kyle.noll@decentlabs.io>
2022-11-17 23:28:30 -0500

    Update location interface on sarco created

--- src/pages/SarcophagusCreatedPage.tsx
+++ src/pages/SarcophagusCreatedPage.tsx
@@ -1,17 +1,12 @@
 import { Center } from '@chakra-ui/react';
 import { SuccessfulCreateSarcophagus } from 'features/embalm/stepContent/components/SuccessfulCreateSarcophagus';
+import { SuccessData } from 'features/embalm/stepContent/hooks/useCreateSarcophagus/useClearSarcophagusState';
 import { useLocation } from 'react-router-dom';
 import { NotFoundPage } from './NotFoundPage';
 
-interface LocationState {
-  successEncryptedShardsTxId: string;
-  successSarcophagusPayloadTxId: string;
-  successSarcophagusTxId: string;
-}
-
 export function SarcophagusCreatedPage() {
   const location = useLocation();
-  const state = location.state as LocationState;
+  const state = location.state as SuccessData;
 
   if (!state) {
     return <NotFoundPage />;
