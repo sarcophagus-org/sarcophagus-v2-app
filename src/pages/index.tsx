3d07d2a089a4994a6b0f8708cc29a6abbd8ee7e8
knoll3 <kyle.noll@decentlabs.io>
2022-11-18 16:13:30 -0500

    Add recipient public key derivation page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,24 +10,27 @@ import { DetailsPage } from './DetailsPage';
 import { EmbalmPage } from './EmbalmPage';
 import { BundlrPage } from './BundlrPage';
 import { TempResurrectionPage } from './TempResurrectionPage';
+import { RecipientsPage } from './RecipientsPage';
 import pharaoh from 'assets/images/pharaoh.gif';
 import { useSupportedNetwork } from 'lib/config/useSupportedNetwork';
 
 export enum RouteKey {
-  EMBALM_PAGE,
-  DASHBOARD_PAGE,
-  DASHBOARD_DETAIL,
   ARCHEOLOGIST_PAGE,
   BUNDLER_PAGE,
+  DASHBOARD_DETAIL,
+  DASHBOARD_PAGE,
+  EMBALM_PAGE,
+  RECIPIENTS,
   TEMP_RESURRECTION_PAGE,
 }
 
 export const RoutesPathMap: { [key: number]: string } = {
-  [RouteKey.EMBALM_PAGE]: '/embalm',
-  [RouteKey.DASHBOARD_PAGE]: '/dashboard',
-  [RouteKey.DASHBOARD_DETAIL]: '/dashboard/:id',
   [RouteKey.ARCHEOLOGIST_PAGE]: '/archaeologists',
   [RouteKey.BUNDLER_PAGE]: '/fundbundlr',
+  [RouteKey.DASHBOARD_DETAIL]: '/dashboard/:id',
+  [RouteKey.DASHBOARD_PAGE]: '/dashboard',
+  [RouteKey.EMBALM_PAGE]: '/embalm',
+  [RouteKey.RECIPIENTS]: '/recipients',
   [RouteKey.TEMP_RESURRECTION_PAGE]: '/temp-resurrection',
 };
 
@@ -80,6 +83,11 @@ export function Pages() {
       element: <TempResurrectionPage />,
       label: 'TempResurrectionPage',
       hidden: true,
+    },
+    {
+      path: RoutesPathMap[RouteKey.RECIPIENTS],
+      element: <RecipientsPage />,
+      label: 'Recipients',
     },
   ];
 
