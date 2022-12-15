6a56f55be952cec6d6304846fe27bc3ae0c33560
Kellar <kellar.base09@gmail.com>
2022-12-15 13:50:24 +0000

    update pending bundlr balance UI

--- src/features/embalm/stepContent/steps/FundBundlr.tsx
+++ src/features/embalm/stepContent/steps/FundBundlr.tsx
@@ -1,3 +1,4 @@
+import { QuestionIcon } from '@chakra-ui/icons';
 import { Text, VStack, Box, Link, Spacer } from '@chakra-ui/react';
 import { Bundlr } from '../components/Bundlr';
 import { BundlrAlertMessage } from '../components/BundlrAlertMessage';
@@ -12,16 +13,14 @@ export function FundBundlr() {
         mb={6}
         variant="secondary"
       >
-        Bundlr is responsible for uploading your file payload to Arweave. You must have enough
-        Bundlr balance to upload the payload. You can connect to Bundlr and fund your account below
-        or on the Bundlr page in the main menu.
+        Bundlr will package your payload and send to Arweave using Ethereum.
         <Spacer />
         <Link
           color="brand.950"
           href="https://bundlr.network/"
           isExternal
         >
-          Learn more
+          <QuestionIcon />
         </Link>
       </Text>
 
