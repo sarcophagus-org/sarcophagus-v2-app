77363d4dc45892dd0a84011b995c27e470ef2ecf
Kellar <kellar.base09@gmail.com>
2022-12-15 12:08:00 +0000

    Update useGetEmbalmerCanClean

--- src/hooks/viewStateFacet/useGetEmbalmerCanClean.ts
+++ src/hooks/viewStateFacet/useGetEmbalmerCanClean.ts
@@ -2,9 +2,13 @@ import { ViewStateFacet__factory } from '@sarcophagus-org/sarcophagus-v2-contrac
 import { BigNumber } from 'ethers';
 import { useNetworkConfig } from 'lib/config';
 import { Sarcophagus } from 'types';
-import { useContractRead } from 'wagmi';
+import { useAccount, useContractRead } from 'wagmi';
 import { useGetGracePeriod } from './useGetGracePeriod';
 
+/**
+ * Uses `embalmerClaimWindow` from the contracts to check if the connected account can clean the
+ * sarcophagus. Returns `false` if the connected account is not the embalmer of the sarcophagus.
+ */
 export function useGetEmbalmerCanClean(sarcophagus: Sarcophagus | undefined): boolean {
   const networkConfig = useNetworkConfig();
   const gracePeriod = useGetGracePeriod();
@@ -15,8 +19,11 @@ export function useGetEmbalmerCanClean(sarcophagus: Sarcophagus | undefined): bo
     functionName: 'getEmbalmerClaimWindow',
   });
 
+  const { address } = useAccount();
+
   if (!data) return false;
   if (!sarcophagus) return false;
+  if (sarcophagus.embalmerAddress !== address) return false;
 
   const cleanWindow = BigNumber.from(data).toNumber();
   const nowSeconds = Math.trunc(Date.now() / 1000);
