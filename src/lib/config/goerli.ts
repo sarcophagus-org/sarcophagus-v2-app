0edaed8071c5b6f9a229788c4b7bd862b9135919
sethhrbek <seth.hrbek@gmail.com>
2022-12-23 17:04:23 -0500

    update dd addy

--- src/lib/config/goerli.ts
+++ src/lib/config/goerli.ts
@@ -5,7 +5,7 @@ export const goerliNetworkConfig: NetworkConfig = {
   networkName: 'Goerli Testnet',
   networkShortName: 'Goerli',
   sarcoTokenAddress: '0x4633b43990b41B57b3678c6F3Ac35bA75C3D8436',
-  diamondDeployAddress: '0x270Ffc6B0e0d8b8F66C30965AF25cB872B2Ee273',
+  diamondDeployAddress: '0x25844741A8223Dc7c5B846E198D479c6993009F7',
   etherscanApiUrl: 'https://api-goerli.etherscan.io/api',
   etherscanApiKey: '',
   explorerUrl: 'https://goerli.etherscan.io/',
