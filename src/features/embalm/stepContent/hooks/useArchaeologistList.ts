ec3afb265c501a56d5a98e18bbfa3da956d9e6e2
sethhrbek <seth.hrbek@gmail.com>
2023-02-21 13:39:56 -0500

    run prettier

--- src/features/embalm/stepContent/hooks/useArchaeologistList.ts
+++ src/features/embalm/stepContent/hooks/useArchaeologistList.ts
@@ -18,7 +18,7 @@ export function useArchaeologistList() {
     unwrapsFilter,
     failsFilter,
     archAddressSearch,
-    showSelectedArchaeologists
+    showSelectedArchaeologists,
   } = useSelector(s => s.archaeologistListState);
 
   const onlineArchaeologists = archaeologists.filter(a => a.isOnline);
@@ -43,7 +43,7 @@ export function useArchaeologistList() {
   const sortOrderByMap: { [key: number]: 'asc' | 'desc' | undefined } = {
     [SortDirection.NONE]: undefined,
     [SortDirection.ASC]: 'asc',
-    [SortDirection.DESC]: 'desc'
+    [SortDirection.DESC]: 'desc',
   };
 
   const handleCheckArchaeologist = useCallback(
@@ -84,7 +84,7 @@ export function useArchaeologistList() {
     if (archaeologistFilterSort.sortDirection !== SortDirection.NONE) {
       return orderBy(
         visibleArchaeologists,
-        function(arch) {
+        function (arch) {
           let sortValue;
           if (archaeologistFilterSort.sortType === SortFilterType.DIGGING_FEES) {
             sortValue = arch.profile.minimumDiggingFeePerSecond;
@@ -142,6 +142,6 @@ export function useArchaeologistList() {
     SortDirection,
     sortedArchaeologist,
     sortedFilteredArchaeologist,
-    unwrapsFilter
+    unwrapsFilter,
   };
 }
