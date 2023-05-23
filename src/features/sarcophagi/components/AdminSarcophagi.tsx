847bc085e0ec74daa236d96f8c7d51465ea57e49
Kellar <kellar.base09@gmail.com>
2023-05-23 17:34:15 +0100

    Update counts to use sdk

--- src/features/sarcophagi/components/AdminSarcophagi.tsx
+++ src/features/sarcophagi/components/AdminSarcophagi.tsx
@@ -1,57 +1,33 @@
 import { RepeatIcon } from '@chakra-ui/icons';
 import { Flex, IconButton, Text } from '@chakra-ui/react';
-import { useNetworkConfig } from 'lib/config';
-import { storeDeploymentBlockNumber } from 'lib/utils/storeDeploymentBlockNumber';
-import { useEffect } from 'react';
-import { useProvider } from 'wagmi';
-import useSarcophagusCount from '../hooks/useSarcophagusCount';
+import { useSarcophagusCount } from '../hooks/useSarcophagusCount';
 
 export function AdminSarcophagi() {
-  const networkConfig = useNetworkConfig();
-  const provider = useProvider();
-  const {
-    fetchEvents,
-    isLoading,
-    currentBlock,
-    blockCount,
-    sarcophagusCount,
-    sarcophagusCountLocalStorageKey,
-  } = useSarcophagusCount();
+  const { counts, loading } = useSarcophagusCount();
 
   async function handleRefresh() {
-    localStorage.removeItem(sarcophagusCountLocalStorageKey);
-    fetchEvents();
+    window.location.reload();
   }
-
-  useEffect(() => {
-    (async () => {
-      await storeDeploymentBlockNumber(provider, networkConfig);
-      await fetchEvents();
-    })();
-  }, [fetchEvents, networkConfig, provider]);
 
   return (
     <Flex direction="column">
-      <Text>This page is a work in progress.</Text>
       <Flex>
-        {isLoading ? (
+        {loading ? (
           <Flex direction="column">
             <Text mt={3}>Searching for Sarcophagi...</Text>
-            <Text>
-              Progress: {currentBlock} out of {blockCount} blocks processed.
-            </Text>
           </Flex>
         ) : (
           <Flex>
             <Text mt={3}>
-              Found {sarcophagusCount} {sarcophagusCount === 1 ? 'sarcophagus' : 'sarocphagi'}.
+              Found {counts.totalSarcophagi}{' '}
+              {counts.totalSarcophagi === 1 ? 'sarcophagus' : 'sarocphagi'}.
             </Text>
             <IconButton
               aria-label="refresh"
               icon={<RepeatIcon />}
               onClick={handleRefresh}
               variant="ghost"
-              isLoading={isLoading}
+              isLoading={loading}
             />
           </Flex>
         )}
