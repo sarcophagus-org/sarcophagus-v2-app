46b64afa9d12615f4ae80007f81a503f5989497b
Seth Hrbek <seth.hrbek@gmail.com>
2023-09-19 10:00:10 -0400

    fix alchemy provider to work with sepolia by using json rpc provider

--- src/lib/network/WalletProvider.tsx
+++ src/lib/network/WalletProvider.tsx
@@ -3,7 +3,7 @@ import { NetworkConfigProvider } from 'lib/config/NetworkConfigProvider';
 import { configureChains, createClient, WagmiConfig } from 'wagmi';
 import { publicProvider } from 'wagmi/providers/public';
 import { infuraProvider } from 'wagmi/providers/infura';
-import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { jsonRpcProvider } from 'wagmi/providers/jsonRpc';
 import { walletConnectionTheme } from '../../theme/walletConnectionTheme';
 import { sepolia, mainnet, goerli, hardhat } from '@wagmi/core/chains';
 
@@ -11,7 +11,12 @@ export function WalletProvider({ children }: { children: React.ReactNode }) {
   const { chains, provider } = configureChains(
     [mainnet, goerli, hardhat, sepolia],
     [
-      alchemyProvider({ apiKey: process.env.REACT_APP_ALCHEMY_API_KEY!, priority: 0 }),
+        jsonRpcProvider({
+            rpc: () => ({
+                http: `https://eth-sepolia.g.alchemy.com/v2/${process.env.REACT_APP_ALCHEMY_API_KEY!}`,
+            }),
+            priority: 0
+        }),
       infuraProvider({ apiKey: process.env.REACT_APP_INFURA_API_KEY!, priority: 1 }),
       publicProvider({ priority: 2 }),
     ]
